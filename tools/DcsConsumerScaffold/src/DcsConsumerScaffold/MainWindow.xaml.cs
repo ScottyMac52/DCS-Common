abc574416a3234fe34383b6f97211cf3fdaa9d84
Scott McIntosh <scottymac40love@gmail.com>
2026-08-05 11:18:59 -0700

    wpf: MainWindow code-behind browse handlers

---
+++ tools/DcsConsumerScaffold/src/DcsConsumerScaffold/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+using System.Windows;
+using DcsConsumerScaffold.ViewModels;
+using Microsoft.Win32;
+
+namespace DcsConsumerScaffold;
+
+public partial class MainWindow : Window
+{
+    private readonly MainViewModel _viewModel = new();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = _viewModel;
+    }
+
+    private void BrowseProfiles_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFolderDialog
+        {
+            Title = "Select DCS joystick profiles directory",
+        };
+        if (dialog.ShowDialog(this) == true)
+        {
+            _viewModel.ProfilesDir = dialog.FolderName;
+        }
+    }
+
+    private void BrowseModifiers_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog
+        {
+            Title = "Select modifiers.lua",
+            Filter = "Lua files (*.lua)|*.lua|All files (*.*)|*.*",
+        };
+        if (dialog.ShowDialog(this) == true)
+        {
+            _viewModel.ModifiersPath = dialog.FileName;
+        }
+    }
+
+    private void BrowseCommon_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFolderDialog
+        {
+            Title = "Select DCS-Common repository root",
+        };
+        if (dialog.ShowDialog(this) == true)
+        {
+            _viewModel.CommonRoot = dialog.FolderName;
+        }
+    }
+}
