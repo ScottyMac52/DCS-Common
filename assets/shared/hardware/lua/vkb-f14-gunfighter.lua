7c2880f75c7b61588bd2490334da22768040987d
Scott McIntosh <scottymac40love@gmail.com>
2026-08-10 19:32:54 -0700

    Update vkb-f14-gunfighter.lua

--- assets/shared/hardware/lua/vkb-f14-gunfighter.lua
+++ assets/shared/hardware/lua/vkb-f14-gunfighter.lua
@@ -5,18 +5,18 @@ return {
   controls = {
     { id = "vkb-trigger", key = "JOY_BTN1", type = "trigger", hardwareLabel = "Trigger" },
     { id = "vkb-btn-release", key = "JOY_BTN3", type = "button", hardwareLabel = "Weapon release" },
-    { id = "vkb-btn-dlc", key = "JOY_BTN5", type = "button", hardwareLabel = "DLC Enage / Disengage" },
     { id = "vkb-pinky", key = "JOY_BTN4", type = "button", hardwareLabel = "Pinky" },
+    { id = "vkb-btn-dlc", key = "JOY_BTN5", type = "button", hardwareLabel = "DLC Enage / Disengage" },
     { id = "vkb-nws", key = "JOY_BTN7", type = "button", hardwareLabel = "NWS / Shift" },
     { id = "vkb-hat", key = "JOY_BTN8", type = "button", hardwareLabel = "Push hat" },
-    { id = "vkb-hat", key = "JOY_BTN10", type = "hat", hardwareLabel = "Trim hat" },
-    { id = "vkb-hat", key = "JOY_BTN11", type = "hat", hardwareLabel = "Trim hat" },
-    { id = "vkb-hat", key = "JOY_BTN9", type = "hat", hardwareLabel = "Trim hat" },
-    { id = "vkb-hat", key = "JOY_BTN12", type = "hat", hardwareLabel = "Trim hat" },
+    { id = "vkb-hat", key = "JOY_BTN9", type = "hat", hardwareLabel = "Trim hat right" },
+    { id = "vkb-hat", key = "JOY_BTN10", type = "hat", hardwareLabel = "Trim hat up" },
+    { id = "vkb-hat", key = "JOY_BTN11", type = "hat", hardwareLabel = "Trim hat down" },
+    { id = "vkb-hat", key = "JOY_BTN12", type = "hat", hardwareLabel = "Trim hat left" },
     { id = "vkb-sw4", key = "JOY_BTN16", type = "switch", hardwareLabel = "Weapons Off" },
     { id = "vkb-sw3", key = "JOY_BTN15", type = "switch", hardwareLabel = "Mode Guns" },
     { id = "vkb-sw2", key = "JOY_BTN14", type = "switch", hardwareLabel = "Mode Sidewinder" },
     { id = "vkb-sw1", key = "JOY_BTN13", type = "switch", hardwareLabel = "Mode Sparrow / Phoenix" },
-    { id = "vkb-axis-dlc", key = "X-ROTATION", type = "axis", hardwareLabel = "DLC" }
+    { id = "vkb-axis-dlc", key = "JOY_RX", type = "axis", hardwareLabel = "DLC" }
   }
 }
