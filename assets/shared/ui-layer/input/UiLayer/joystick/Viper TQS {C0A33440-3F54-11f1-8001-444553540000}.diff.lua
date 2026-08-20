d9994b6a59e5c16b2dbcaf4172acacad2c846918
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-08-20 07:41:09 -0700

    feat: migrate canonical UI Layer input payload

---
+++ assets/shared/ui-layer/input/UiLayer/joystick/Viper TQS {C0A33440-3F54-11f1-8001-444553540000}.diff.lua
@@ -0,0 +1,16 @@
+local diff = {
+	["keyDiffs"] = {
+		["d2605pnilu2605cdnilvd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN16",
+					["reformers"] = {
+						[1] = "VKB_F14_BTN7",
+					},
+				},
+			},
+			["name"] = "toggle VR Spyglass Zoom",
+		},
+	},
+}
+return diff
